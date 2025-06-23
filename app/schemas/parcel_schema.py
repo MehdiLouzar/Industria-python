0f7731b3b321bf0b05a9d2143a6b00a86553dfeb
Mehdi LOUZAR <87854904+MehdiLouzar@users.noreply.github.com>
2025-06-23 16:48:21 +0100

    fix: make zone and parcel ids optional

--- app/schemas/parcel_schema.py
+++ app/schemas/parcel_schema.py
@@ -5,6 +5,8 @@ from ..models import Parcel
 from ..utils import point_from_lambert, lambert_from_point
 
 class ParcelSchema(SQLAlchemyAutoSchema):
+    id = fields.Int(dump_only=True)
+    entity_type = fields.Str(dump_only=True)
     # Override des Numeric → Float
     area = fields.Float()
     CoS = fields.Float(attribute="CoS")  # ou 'CoS' si c'est bien le nom de l'attribut
