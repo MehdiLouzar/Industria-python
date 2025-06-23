15df09170d693f3b9205d45ffe05fbd1e8250da6
Mehdi LOUZAR <87854904+MehdiLouzar@users.noreply.github.com>
2025-06-23 16:31:05 +0100

    Allow geometry loading for Lambert coords

--- app/schemas/zone_schema.py
+++ app/schemas/zone_schema.py
@@ -1,16 +1,21 @@
-from marshmallow import fields
+from marshmallow import fields, pre_load
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from geoalchemy2.shape import to_shape
 from ..models import Zone
+from ..utils import point_from_lambert, lambert_from_point
 
 class ZoneSchema(SQLAlchemyAutoSchema):
     # Conversion du total_area (Numeric) en float
     total_area = fields.Float()
 
     # Sérialisation de la géométrie héritée (SpatialEntity.geometry)
-    geometry = fields.Method("get_geometry", dump_only=True)
+    geometry = fields.Method("get_geometry", deserialize="pass_through")
     # Sérialisation du centroïde
     centroid = fields.Method("get_centroid", dump_only=True)
+    lambert_x = fields.Method("get_lambert_x", dump_only=True, allow_none=True)
+    lambert_y = fields.Method("get_lambert_y", dump_only=True, allow_none=True)
+    lambert_x_input = fields.Float(load_only=True, data_key="lambert_x")
+    lambert_y_input = fields.Float(load_only=True, data_key="lambert_y")
 
     class Meta:
         model = Zone
@@ -25,3 +30,26 @@ class ZoneSchema(SQLAlchemyAutoSchema):
     def get_centroid(self, obj):
         shapely_centroid = to_shape(obj.centroid)
         return shapely_centroid.__geo_interface__
+
+    @pre_load
+    def convert_lambert(self, data, **kwargs):
+        x = data.pop("lambert_x", None)
+        y = data.pop("lambert_y", None)
+        if x is not None and y is not None:
+            data["geometry"] = point_from_lambert(x, y)
+        return data
+
+    def pass_through(self, value):
+        return value
+
+    def get_lambert_x(self, obj):
+        if obj.geometry is None:
+            return None
+        x, _ = lambert_from_point(to_shape(obj.geometry))
+        return x
+
+    def get_lambert_y(self, obj):
+        if obj.geometry is None:
+            return None
+        _, y = lambert_from_point(to_shape(obj.geometry))
+        return y
